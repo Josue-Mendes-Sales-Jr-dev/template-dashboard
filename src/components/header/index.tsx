00a900aaf1515427615ab1b001b9fbf64a692a98
Josué Mendes <109279870+Josue-Mendes-Sales-Jr-dev@users.noreply.github.com>
2023-08-26 20:25:42 -0300

    add new config 4

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,6 +5,7 @@ import { BsSearch } from "react-icons/bs"
 import { AiOutlineExpand } from "react-icons/ai"
 import { AiOutlineGlobal } from "react-icons/ai"
 import { AiTwotoneAlert } from "react-icons/ai"
+import {AiOutlineDollar } from "react-icons/ai"
 
 interface IHeaderProps{
   className: string
@@ -15,25 +16,27 @@ export const Header=(props:IHeaderProps)=>{
         <>
         <div className={`${props.className} px-4 bg-gray-900  text-white flex items-center justify-between`}>
           <form className="flex items-center gap-5">
-            <Link href='/'>Dashboard</Link>
+            <Link href='/'><AiOutlineDollar/></Link>
             <div className="flex items-center justify-center"> 
-              <Input type="text" placeHolder="search..." className="h-[30px] rounded-bl-lg rounded-tl-lg text-black pl-3"/>
+              <Input type="text" placeHolder="search..." className="h-[30px] w-[10rem] sm:w-[12rem] rounded-bl-lg rounded-tl-lg text-black pl-3"/>
               <span className="w-[1.5rem] rounded-br-lg rounded-tr-lg h-[30px] bg-white flex items-center justify-center">
                 <BsSearch className="text-gray-800 "/>
               </span>
             </div>
           </form>
-          <div className="flex gap-4 items-center justify-center">
+          <div className="flex gap-4 sm:gap-7 items-center justify-center">
             <span className="hidden sm:block">
               <AiTwotoneAlert/>
             </span>
-            <span className="hidden sm:block">
+            <span >
             <AiOutlineGlobal/>
             </span>
-            <span>
+            <span className="hidden sm:block">
               <AiOutlineExpand/>
             </span>
-            Welcome Junior.
+            <span className=" bg-white w-[1.4rem] h-[1.4rem] rounded-full">
+              
+            </span>
           </div>
         </div>
         </>
